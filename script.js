0588b7be3548d49f94a949d5fb974992c0ee9bf6
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-10-18 10:00:40 -0500

    Set up initial functions

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log('hello');
+function gameBoard() {
+    const spaces = document.querySelectorAll('.space');
+    spaces.forEach((space) => {
+        space.addEventListener('click', () => {
+
+        })
+    })
+}
+
+function players() {
+
+    const playerX = {
+        name: 'Player 1',
+        symbol: 'X'
+    }
+
+    const playerO = {
+        name: 'Player 2',
+        symbol: 'O'
+    }
+    
+    let activePlayer = playerX;
+
+    const switchPlayers = (activePlayer) => {
+        if(activePlayer.name == 'Player 1'){
+            activePlayer = playerO;
+        }else if(activePlayer.name == 'Player 2'){
+            activePlayer = playerX;
+        }
+    }
+
+
+}
+
+function game() {
+
+    const playRound = (space) => {
+        if(!space.innerText){
+            space.innerText = `${activePlayer.symbol}`;
+            players.switchPlayers();
+        }
+    }
+    
+}
+
